59a5b38f82fa6eea648f726ad5f5a543485fd1ba
root <root@localhost.localdomain>
2019-06-24 14:55:51 +0800

    add misc

---
+++ syb/misc/1-1.py
@@ -0,0 +1,11 @@
+import requests,time
+
+url='http://ctf5.shiyanbar.com/misc/keys/keys.php?key=1561358835'
+
+s=requests.session()
+while(1):
+    res=s.get(url)
+    print res.content
+    if res.content.find('false')==-1:
+        break
+    time.sleep(1)
