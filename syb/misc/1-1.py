4c5a0fde6801b2d0d2bd98bebe178f3414ff23c8
aizen167 <18663729918@163.com>
2019-06-25 08:55:21 +0800

    1-1.py

--- syb/misc/1-1.py
+++ syb/misc/1-1.py
@@ -1,11 +1,12 @@
-import requests,time
+import requests
+import time
 
-url='http://ctf5.shiyanbar.com/misc/keys/keys.php?key=1561358835'
+url = 'http://ctf5.shiyanbar.com/misc/keys/keys.php?key=1561358835'
 
-s=requests.session()
+s = requests.session()
 while(1):
-    res=s.get(url)
+    res = s.get(url)
     print res.content
-    if res.content.find('false')==-1:
+    if res.content.find('false') == -1:
         break
     time.sleep(1)
